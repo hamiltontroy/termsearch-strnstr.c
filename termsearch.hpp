d8c5e9b263a2dc75e522013af0df5b6d431716c2
Troy Hamilton <110515824+hamiltontroy@users.noreply.github.com>
2022-12-31 19:55:21 -0600

    This is a version for C++ that is much easier to work with.

---
+++ termsearch.hpp
@@ -0,0 +1,48 @@
+template <typename T, typename N>
+const T *termsearch(const T *bucket, const T *term, const N bucketlen, const N termlen)
+{
+    if(termlen > bucketlen) // prevents a waste of time
+        return nullptr;
+    
+    // if searching for the first occurence of a specific character
+    if(termlen == 1)
+    {
+        for(int k = 0; k < bucketlen; k++)
+        {
+            if(bucket[k] == term[0])
+                return &bucket[k];
+        }
+    }
+    
+    const T *termstart = nullptr;
+    N i = 0, j;
+    
+    RESTART:
+    
+    // stops when the start of term is found
+    for(; i < bucketlen - termlen; i++)
+    {
+        if(bucket[i] == term[0])
+        {
+            termstart = &bucket[i];
+            break;
+        }
+    }
+    
+    // prevents an out of bounds search,
+    // also returns nullptr if term not found
+    if(i >= bucketlen - termlen)
+        return nullptr;
+    
+    // continues to iterate the bucket until the entire length of term is found
+    for(j = 0; j < termlen; j++)
+    {
+        if(termstart[j] != term[j])
+            goto RESTART;
+        
+        if(j == termlen)
+            break;
+    }
+    
+    return termstart;
+}
